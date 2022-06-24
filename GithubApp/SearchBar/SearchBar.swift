c814950c11f7f60a43cd0ad8c2e7f8f6273deffe
devilzcough <devilzcough@gmail.com>
2022-06-24 22:31:26 +0900

    feat :⚡️ SearchBar 부분을 MVVM으로 고쳐보기
    - 이전에 SearchTableViewController 에서 SearchController 를 넣어줬는데,
      해당 뷰를 SearchBar라는 클래스로 분리시킴
    - SearchBar 뷰와 관련된 ViewModel을 만들고 검색어와 검색타입을 받을
      PublishRelay와 검색 버튼을 눌렀을 때 받을 PublishRelay를 만듬
    - 또한 검색 버튼이 눌렸을 때, 작성된 검색어와 타입을 내보낼 수 있도록
      구현
    - SearchBar 에서는 해당 viewModel을 주입받아 searchBar의 검색 내용,
      scopeButton, search Button 등을 bind 하였고, 검색 버튼이 눌렸을 때
    키보드가 내려가도록 구현

---
+++ GithubApp/SearchBar/SearchBar.swift
@@ -0,0 +1,8 @@
+//
+//  SearchBar.swift
+//  GithubApp
+//
+//  Created by jiniz.ll on 2022/06/24.
+//
+
+import Foundation
---
+++ GithubApp/SearchBar/SearchBarViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchBarViewModel.swift
+//  GithubApp
+//
+//  Created by jiniz.ll on 2022/06/24.
+//
+
+import Foundation
